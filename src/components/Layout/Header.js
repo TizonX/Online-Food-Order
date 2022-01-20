971f21870e300c1b43e7e6ec86c517c1f8c9d661
GitVishal <vishalmishra6604@gmail.com>
2022-01-20 18:00:08 +0530

    project setup

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.modue.css"
+function Header() {
+    return (
+        <>
+            <header></header>
+            <div></div>
+        </>
+    )
+}
+
+export default Header
